ac1e91963c95c7ae1a89cbb3af47e94ab49e695f
Clarisson <clarisson.bento@ufv.br>
2023-11-21 17:03:23 -0300

    Modo arquivo corrigido

--- por_arquivo.c
+++ por_arquivo.c
@@ -1,6 +1,8 @@
 #include "baralho.h"
 #include "ordenadores.h"
 
+int extraiTipo(char tipo[20]);
+
 void modo_arquivo(){
     FILE *arquivo;
     arquivo = fopen("TESTE.txt", "r");
@@ -8,41 +10,72 @@ void modo_arquivo(){
     Baralho baralho;
     Hand mao;
 
-    int N;
-    int numero;
-    char cor[20];
-    char tipo[20];
+    int N, numero;
+    char cor[20], tipo[20];
 
     fscanf(arquivo, "%i", &N);
     printf("\nMãos de cartas: %i\n\n", N);
 
     for (int i = 0; i < N; i++){
-        printf("[%i]: ", i);
-        for (int j = 0; j < 10; j++){
+        printf("\n[%i mão]: ", i+1);
+        for (int j = 0; j < MAO; j++){
 
-            fscanf(arquivo, " %s %s ", cor, tipo);
-            printf("%s ", cor);
+            fscanf(arquivo, " (%s %s) ", cor, tipo);
 
-            if(strcmp(cor, "verde") == 0){
-                mao.cartas[j].i_carta = numero;
+            if(strcmp(cor, "Verde") == 0){
                 mao.cartas[j].cor = VERDE;
+                numero = extraiTipo(tipo);
+                mao.cartas[j].numero = numero;
+                mao.cartas[j].i_carta = numero;
             }
-            if(strcmp(cor, "amarelo") == 0){
-                mao.cartas[j].i_carta = numero + 14;
+            if(strcmp(cor, "Amarelo") == 0){
                 mao.cartas[j].cor = AMARELO;
+                numero = extraiTipo(tipo);
+                mao.cartas[j].numero = numero;
+                mao.cartas[j].i_carta = numero + 13;
             }
-            if(strcmp(cor, "vermelho") == 0){
-                mao.cartas[j].i_carta = numero + 27;
+            if(strcmp(cor, "Vermelho") == 0){
                 mao.cartas[j].cor = VERMELHO;
+                numero = extraiTipo(tipo);
+                mao.cartas[j].numero = numero;
+                mao.cartas[j].i_carta = numero + 26;
             }
-            if(strcmp(cor, "azul") == 0){
-                mao.cartas[j].i_carta = numero + 40;
+            if(strcmp(cor, "Azul") == 0){
                 mao.cartas[j].cor = AZUL;
+                numero = extraiTipo(tipo);
+                mao.cartas[j].numero = numero;
+                mao.cartas[j].i_carta = numero + 39;
             }
+            if(strcmp(cor, "Preto") == 0){
+                mao.cartas[j].cor = PRETO;
+                numero = extraiTipo(tipo);
+                mao.cartas[j].numero = numero;
+                mao.cartas[j].i_carta = numero + 39;
+            }
+
         }
         printf("\n");
-
+        exibeMao(&mao);
     }
-
+    
     fclose(arquivo);
+}
+
+// Função que lê as cartas especiais "Pular" / "Voltar" e converte em numero 
+int extraiTipo(char tipo[20]){
+    int numero;
+
+    if (strcmp(tipo, "Pular)") == 0){
+        numero = 10;
+    }else if (strcmp(tipo, "Voltar)") == 0){
+        numero = 11;
+    }else if (strcmp(tipo, "+2)") == 0){
+        numero = 12;
+    }else if (strcmp(tipo, "+4)") == 0){
+        numero = 13;
+    }else if (strcmp(tipo, "Coringa)") == 0){
+        numero = 15;
+    }else numero = atoi(tipo);
+
+    return numero;
 }
